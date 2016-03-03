fc918273305c28601a79d9db169ed7234b062f9a
Chin Perera <Chinthana.Perera@lv.com>
2016-03-03 16:13:44 +0000

    Chin Perera : Added grunt autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,10 @@
 module.exports = function( grunt ) {
-    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks("grunt-contrib-cssmin");
-
+    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks('grunt-postcss');
 
     grunt.initConfig({
-        copy: {
-            js:{
-                src: ['js/**/*.js'],
-                dest: 'dist/js/'
-            }
-        },
-        clean: {
-            dist: {
-                src: ["src/main/webapp/dist/"]
-            }
-        },
         cssmin: {
             options: {
                 advanced: false,
@@ -24,29 +12,42 @@ module.exports = function( grunt ) {
             },
             minify: {
                 expand: true,
-                cwd: 'src/main/webapp/css/lv/',
+                cwd: 'src/css',
                 src: ['*.css', '*.css'],
-                dest: 'src/main/webapp/css/lv'
+                dest: 'src/css'
             }
         },
         watch: {
             sass: {
-                files: ['src/sass-rebrand/**/*.scss','src/main/webapp/WEB-INF/views/**/*.html', 'src/main/webapp/js/**/*.js'],
-                tasks: ['autoprefixer', 'cssmin']
+                files: ['sass/**/*.scss', 'src/js/**/*.js'],
+                tasks: ['sass', 'autoprefixer', 'cssmin']
             }
         },
-
-        autoprefixer: {
-            options: {
-                browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Firefox >= 28', 'Android >= 2.3']
-            },
-            single_file: {
-                src: 'src/main/webapp/css/lv/styles.css'
+        sass: {
+            dist: {
+                files: {
+                    'src/css/styles.css': 'sass/**/styles.scss',
+                    'src/css/ie.css': 'sass/**/ie.scss'
+                }
             }
+        },
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer-core')({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Firefox >= 28', 'Android >= 2.3']})
+                ]
+            },
+            multiple_files: [{
+                expand: true,
+                flatten: true,
+                src: 'src/css/*.css', // -> src/css/file1.css, src/css/file2.css
+                dest: 'dist/css/' // -> dest/css/file1.css, dest/css/file2.css
+            }]
         }
     });
 
-    grunt.registerTask("dist", ["clean", "cssmin"]);
+    grunt.registerTask("dist", ["sass", "postcss", "cssmin"]);
     grunt.registerTask("default", ["dist"]);
 
 };
